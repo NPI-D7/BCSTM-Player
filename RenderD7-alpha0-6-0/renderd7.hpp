12645801ca203a68d9ac5f48cc1445bd2660d2f8
Tobi <tobid7@outlook.de>
2021-06-22 06:57:37 +0200

    Update renderd7.hpp

--- RenderD7-alpha0-6-0/renderd7.hpp
+++ RenderD7-alpha0-6-0/renderd7.hpp
@@ -20,7 +20,7 @@
 #include "parameter.hpp"
 #include "thread.hpp"
 
-#define RENDERD7VSTRING "0.6.2"
+#define RENDERD7VSTRING "6.１.0"
 #define CHANGELOG "6.1.0: rewrite Thread system/n6.0.2: Fix Code in lang.hpp\nadd Draw Text Left Function.\nadd changelog\n6.0.1: add Threading system."
 #define DEFAULT_CENTER 0.5f
 
