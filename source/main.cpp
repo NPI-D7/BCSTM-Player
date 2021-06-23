b59db8988657653116ff72e4fb01172177d64892
Tobi <tobid7@outlook.de>
2021-06-23 19:29:14 +0200

    Update main.cpp
    Add fps

--- source/main.cpp
+++ source/main.cpp
@@ -12,6 +12,8 @@ void CardScan(RenderD7::Parameter param) {
     int id = param.get<int>();
 
     while (true) {
+        RenderD7::OnScreen(Bottom);
+        RenderD7::DrawTextLeft(395, 2, 0.6f, RenderD7::Color::Hex("#ffffff), "Fps: " + RenderD7::GetFramerate());
         D7TM::CardLoop();
         RenderD7::Thread::sleep(1000 * id); // wait; also, this is needed to allow for concurrency (refer to the documentation for m3d::Thread::sleep())
     }
