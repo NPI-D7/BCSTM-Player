da5c6ead9eeacdf89de062a3f345ffe8f353b6a4
Tobi <tobid7@outlook.de>
2022-04-13 02:56:11 +0200

    Update BCSTM.hpp

--- BCSTM/BCSTM.hpp
+++ BCSTM/BCSTM.hpp
@@ -20,7 +20,7 @@ public:
         inline std::string GetLoopStart(){ return std::to_string(this->m_blockLoopStart);}
         inline std::string GetLoopEnd(){ return std::to_string(this->m_blockLoopEnd);}
         inline float GetTotal(){ return (float)this->lastTime;}
-        inline float GetCurrent(){ return (float)(this->currTime;}
+        inline float GetCurrent(){ return (float)this->currTime;}
 
 
 protected:
