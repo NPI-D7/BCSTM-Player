a322ca08f4a147aca93cceb4a2ffa3847fd16957
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-04-14 21:08:26 +0200

    __

--- D7-Menu-Core/SheetMaker.cpp
+++ D7-Menu-Core/SheetMaker.cpp
@@ -38,7 +38,7 @@ void SheetMaker::Write(std::string path)
     const u32 srcPos = ((((y >> 3) * (width >> 3) + (x >> 3)) << 6) +
 	((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) |
 	((x & 4) << 2) | ((y & 4) << 3))) * 4;
-    const u32 dstPos = (y * zwidth + x) * 4;
+    const u32 dstPos = (y * 1024 + x) * 4;
     ImageBuffer[dstPos + 0] = 0;
     ImageBuffer[dstPos + 1] = 255;
     ImageBuffer[dstPos + 2] = 0;
