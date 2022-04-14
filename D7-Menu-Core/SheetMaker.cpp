a9d7417269ffc57a82250837f7755ba8c5626352
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-04-14 16:32:46 +0200

    Stealing Code rom RenderD7 wasnt a good idea

--- D7-Menu-Core/SheetMaker.cpp
+++ D7-Menu-Core/SheetMaker.cpp
@@ -16,10 +16,10 @@ void SheetMaker::AddInage(int zwidth, int zheight, C2D_Image *image)
 {
     for(unsigned y = count*zheight; y < height; y++){
     for(unsigned x = count*zwidth; x < width; x++) {
-    ImageBuffer[4 * width * y + 4 * x + 0] = ((uint8_t *)this->img.tex->data)[dstPos + 3];
-    ImageBuffer[4 * width * y + 4 * x + 1] = ((uint8_t *)this->img.tex->data)[dstPos + 2];
-    ImageBuffer[4 * width * y + 4 * x + 2] = ((uint8_t *)this->img.tex->data)[dstPos + 1];
-    ImageBuffer[4 * width * y + 4 * x + 3] = ((uint8_t *)this->img.tex->data)[dstPos + 0];
+    ImageBuffer[4 * width * y + 4 * x + 0] = ((uint8_t *)img->tex->data)[dstPos + 3];
+    ImageBuffer[4 * width * y + 4 * x + 1] = ((uint8_t *)img->tex->data)[dstPos + 2];
+    ImageBuffer[4 * width * y + 4 * x + 2] = ((uint8_t *)img->tex->data)[dstPos + 1];
+    ImageBuffer[4 * width * y + 4 * x + 3] = ((uint8_t *)img->tex->data)[dstPos + 0];
     }}
     count++;
 }
