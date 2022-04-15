bce562407ceb6e9752a8af3875e65d76caf97d89
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-04-15 23:36:30 +0200

    ___

--- D7-Menu-Core/SheetMaker.cpp
+++ D7-Menu-Core/SheetMaker.cpp
@@ -14,12 +14,12 @@ SheetMaker::~SheetMaker()
 
 }
 
-uint32_t* SheetMaker::ToRGBA888(uint16_t* bufdata, int ywidth, int yheight)
+uint32_t SheetMaker::ToRGBA888(uint16_t* bufdata, int ywidth, int yheight)
 {
-    uint8_t r = (((((int)bufdata >> 11) & 0x1F) * 527) + 23) >> 6;
-    uint8_t g = (((((int)bufdata >> 5) & 0x1F) * 259) + 33) >> 6;
-    uint8_t b = ((((int)bufdata & 0x1F) & 0x1F) + 23) >> 6;
-    return RGBA8((int)r,(int)g,(int)b,255);    
+    int r = (((((int)bufdata >> 11) & 0x1F) * 527) + 23) >> 6;
+    int g = (((((int)bufdata >> 5) & 0x1F) * 259) + 33) >> 6;
+    int b = ((((int)bufdata & 0x1F) & 0x1F) + 23) >> 6;
+    return RGBA8(r,g,b,255);    
 }
 
 void SheetMaker::AddInage(int zwidth, int zheight, u16* buf)
@@ -29,7 +29,7 @@ void SheetMaker::AddInage(int zwidth, int zheight, u16* buf)
         count++;
         count2 = 0;
     }
-    u32* imagesrc = this->ToRGBA888(buf, zwidth, zheight);
+    u32* imagesrc = (u32*)this->ToRGBA888(buf, zwidth, zheight);
     for(unsigned y = count*zheight; y < height; y++){
     for(unsigned x = count2*zwidth; x < width; x++) {
     const u32 srcPos = ((((y >> 3) * (width >> 3) + (x >> 3)) << 6) +
