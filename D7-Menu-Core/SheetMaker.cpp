2f73949e898a9436dbc8e3627d64a582b1b7ed3e
Tobi-D7 <tobid7@outlook.de>
2022-04-16 01:32:20 +0200

    __

--- D7-Menu-Core/SheetMaker.cpp
+++ D7-Menu-Core/SheetMaker.cpp
@@ -1,5 +1,10 @@
 #include "SheetMaker.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 #define RGBA8(r, g, b, a) ((((r) & 0xFF) << 0) | (((g) & 0xFF) << 8) | (((b) & 0xFF) << 16) | (((a) & 0xFF) << 24))
 
 SheetMaker::SheetMaker()
@@ -29,18 +34,20 @@ void SheetMaker::AddInage(int zwidth, int zheight, u16* buf)
         count++;
         count2 = 0;
     }
-    u32* imagesrc = (u32*)this->ToRGBA888(buf, zwidth, zheight);
-    for(unsigned y = count*zheight; y < height; y++){
+    unsigned char* convi = stbi__convert_16_to_8(buf, zwidth, zheight, 8);
+    stbi_write_jpg("sdmc:/test.jpg", 48, 48, 1, convi, 20);
+    //u32* imagesrc = (u32*)this->ToRGBA888(buf, zwidth, zheight);
+    /*for(unsigned y = count*zheight; y < height; y++){
     for(unsigned x = count2*zwidth; x < width; x++) {
     const u32 srcPos = ((((y >> 3) * (zwidth >> 3) + (x >> 3)) << 6) +
 	((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) |
 	((x & 4) << 2) | ((y & 4) << 3))) * 4;
     const u32 dstPos = (y * zwidth + x) * 4;
-    ImageBuffer[dstPos + 0] = ((uint8_t *)imagesrc)[srcPos + 0];
-    ImageBuffer[dstPos + 1] = ((uint8_t *)imagesrc)[srcPos + 1];
-    ImageBuffer[dstPos + 2] = ((uint8_t *)imagesrc)[srcPos + 2];
-    ImageBuffer[dstPos + 3] = ((uint8_t *)imagesrc)[srcPos + 3];
-    }}
+    ImageBuffer[dstPos + 0] = ((uint8_t *)convi)[srcPos + 0];
+    ImageBuffer[dstPos + 1] = ((uint8_t *)convi)[srcPos + 1];
+    ImageBuffer[dstPos + 2] = ((uint8_t *)convi)[srcPos + 2];
+    ImageBuffer[dstPos + 3] = ((uint8_t *)convi)[srcPos + 3];
+    }}*/
     count2++;
 }
 
