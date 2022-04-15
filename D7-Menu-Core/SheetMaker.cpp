fbc52b9efecdd84ddfeb9e5fb0fc27183528e9b5
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-04-15 23:52:00 +0200

    __

--- D7-Menu-Core/SheetMaker.cpp
+++ D7-Menu-Core/SheetMaker.cpp
@@ -32,7 +32,7 @@ void SheetMaker::AddInage(int zwidth, int zheight, u16* buf)
     u32* imagesrc = (u32*)this->ToRGBA888(buf, zwidth, zheight);
     for(unsigned y = count*zheight; y < height; y++){
     for(unsigned x = count2*zwidth; x < width; x++) {
-    const u32 srcPos = ((((y >> 3) * (width >> 3) + (x >> 3)) << 6) +
+    const u32 srcPos = ((((y >> 3) * (zwidth >> 3) + (x >> 3)) << 6) +
 	((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) |
 	((x & 4) << 2) | ((y & 4) << 3))) * 4;
     const u32 dstPos = (y * zwidth + x) * 4;
